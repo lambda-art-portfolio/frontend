c7461afeb82d58b28f4d33bc0145ad55869f8e53
Irving Duran <ijd.irving@gmail.com>
2019-04-15 18:33:37 -0300

    attempting to add error in signup

--- src/components/routes/signup.js
+++ src/components/routes/signup.js
@@ -6,22 +6,75 @@ import Login from "./Login";
 export default class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       credentails: {
-        email:"",
-        username:"",
-        password:"",
-        
+        email: "",
+        username: "",
+        password: "",
+        errors: []
       }
-    }
+    };
+  }
+
+  showValidationErr(e, msg) {
+    this.setState(prevState => ({
+      errors: [...prevState.credentails.errors, { e, msg }]
+    }));
+  }
+  clearValidationErr(e) {
+    this.setState(prevState => {
+      let newArr = [];
+      for (let err of prevState.credentails.errors) {
+        if (e !== err.e) {
+          newArr.push(err);
+        }
+      }
+      return newArr;
+    });
+  }
+
+  onUsernameChange(e) {
+    this.setState.credentails({ username: e.target.value });
+    this.clearValidationErr("username");
+  }
+  onEmailChange(e) {
+    this.setState.credentails({ email: e.target.value });
+    this.clearValidationErr("email");
+  }
+  onPasswordChange(e) {
+    this.setState.credentails({ password: e.target.value });
+    this.clearValidationErr("password");
   }
 
   register() {
-  
     // console.log('this.state', this.state)
+    if (this.state.credentails.email === "") {
+      return this.showValidationErr("username", "Username cannot be empty")
+    } else  if (this.state.credentails.username === "") {
+      return this.showValidationErr("username", "Username cannot be empty")
+    }else  if (this.state.credentails.password === "") {
+      return this.showValidationErr("username", "Username cannot be empty")
+    }
   }
 
+
+
   render() {
+    let usernameErr= null,
+    passwordErr= null,
+    emailErr= null;
+
+    for(let err of this.state.credentails.errors) {
+      if(err.e ==="username") {
+        usernameErr = err.msg;
+      } if (err.e ==="email") {
+        emailErr = err.msg;
+      } if (err.e ==="password") {
+        passwordErr = err.msg;
+      }
+
+    }
+
     return (
       <div className="signupContainer">
         <div className="buttonContainer">
@@ -33,36 +86,41 @@ export default class Signup extends Component {
 
         <div className="formContainer">
           <h1>Sign up</h1>
-          <form className="signupForm"
-          >
+          <form className="signupForm">
             <input
               className="signupInput"
               type="text"
               name="email"
               placeholder="Email"
               // value=''
-              // onChange=''
+              onChange={this.onEmailChange}
             />
+            <p>{emailErr ? emailErr : "" }</p>
             <input
               className="signupInput"
               type="text"
               name="username"
               placeholder="Username"
               // value=''
-              // onChange=''
+              onChange={this.onUsernameChange}
             />
+            <p>{usernameErr ? usernameErr : "" }</p>
             <input
               className="signupInput"
               type="password"
-              name="passwprd"
+              name="password"
               placeholder="Password"
               // value=''
-              // onChange=''
+              onChange={this.onPasswordChange}
             />
+            <p>{passwordErr ? passwordErr : "" }</p>
             {/* <Link to="/login"> */}
-              <button className="formButton"
-              //  onClick=''
-              >Signup</button>
+            <button
+              className="formButton"
+               onClick={this.register}
+            >
+              Signup
+            </button>
             {/* </Link> */}
           </form>
         </div>
