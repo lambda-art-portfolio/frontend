667e5f827de0df42246a60b23d5be0f92d843b9a
Irving Duran <ijd.irving@gmail.com>
2019-04-18 19:29:55 -0300

    final maybe

--- src/components/navigation/ProtectedNav.js
+++ src/components/navigation/ProtectedNav.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "../../CSS/nav.css";
 import { Link } from "react-router-dom";
+import account from "../../Images/account.png";
+import logout from "../../Images/logout.png";
+import home from "../../Images/home.png";
+
 
 export default class ProtectedNav extends Component {
-
   logout = () => {
-    localStorage.removeItem("token");        
+    localStorage.removeItem("token");
     localStorage.removeItem("username");
   };
 
@@ -18,19 +21,28 @@ export default class ProtectedNav extends Component {
           <nav>
             <div>
               <Link className="link home" to="/">
-              Artfolio
+                Artfolio
               </Link>
             </div>
             <div>{/* <input /> */}</div>
             <div>
-              <Link className="link user" to="/protected">
+              {/* <Link className="link user" to="/protected">
                 User Page
+              </Link> */}
+              <Link className="link login" to="/">
+                <img src={home} alt="" className="home-logo" />
+              </Link>
+              <Link className="link login" to="/protected">
+                <img src={account} alt="" className="account-logo" />
               </Link>
               {/* <Link className="link account" to="/account">
                 Account
               </Link> */}
-              <Link to="/" className="link logout" onClick={this.logout}>
+              {/* <Link to="/" className="link logout" onClick={this.logout}>
                 Logout
+              </Link> */}
+              <Link className="link login" to="/" onClick={this.logout}>
+                <img src={logout} alt="" className="account-logo" />
               </Link>
             </div>
           </nav>
